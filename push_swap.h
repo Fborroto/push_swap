4ceb1378e5d0d07425f7bd82e5602a8b383ab9fe
fborroto <borrotofrancesco@gmail.com>
2023-07-27 00:36:11 +0200

    primo push

---
+++ push_swap.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fborroto <fborroto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/26 23:55:03 by fborroto          #+#    #+#             */
+/*   Updated: 2023/07/27 00:06:27 by fborroto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "stdlib.h"
+# include "stdio.h"
+# include "unistd.h"
+# include "stdbool.h"
+
+typedef struct s_stack
+{
+	int				value;
+	int				index;
+	int				final_index;
+	bool			above_median;
+	bool			cheapest;
+	struct s_stack	*target_node;
+	struct s_stack	*next;
+	struct s_stack	*prev;
+}					t_stack;
+
+
+
+#endif
